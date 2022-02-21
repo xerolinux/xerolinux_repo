2efeca27cab0d2ac8d0c9f1eaa11dd09fc0e510f
TechXero <steve@techxero.com>
2022-02-21 23:09:59 +0200

    Repo Fix

--- x86_64/update_repo.sh
+++ x86_64/update_repo.sh
@@ -5,6 +5,16 @@ rm xerolinux_repo*
 echo "repo-add"
 repo-add -n -R xerolinux_repo.db.tar.gz *.pkg.tar.zst
 
+sleep 1
+
+rm xerolinux_repo.db
+
+rm xerolinux_repo.files
+
+mv xerolinux_repo.db.tar.gz xerolinux_repo.db
+
+mv xerolinux_repo.files.tar.gz xerolinux_repo.files
+
 echo "####################################"
 echo "Repo Updated!!"
 echo "####################################"
